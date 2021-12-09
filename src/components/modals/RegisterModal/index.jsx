0cf489892993f96a8296f72241772ed035140fca
Gabriel Soares <gabrielsoaresii@outlook.com>
2021-12-09 07:44:16 -0300

    add: form submit

--- src/components/modals/RegisterModal/index.jsx
+++ src/components/modals/RegisterModal/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Modal,
   ModalOverlay,
@@ -8,15 +9,52 @@ import {
   FormControl,
   FormLabel,
   Button,
-  Input
+  Input,
+  useToast
 } from "@chakra-ui/react";
 
 import { useModal } from '../../../context/ModalRegister';
 
+import styles from './index.module.css';
+
 export default function RegisterModal() {
+  const message = useToast();
+  const nameRef = React.useRef();
+  const phoneRef = React.useRef();
+  const emailRef = React.useRef();
+  const [loading, setLoading] = React.useState(false);
   const { isOpen, setIsOpen } = useModal();
 
   const onClose = () => setIsOpen(false);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    const url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeElrfdYBJtxUdzS5IG78Ea0aZwfYsSduaEroesNayIui3B8w/formResponse";
+
+    const payload = new FormData();
+    payload.append("entry.1797406478", nameRef.current.value);
+    payload.append("entry.1763012668", phoneRef.current.value);
+    payload.append("entry.1549734300", emailRef.current.value);
+
+    const headers = new Headers();
+    headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+    headers.append('Access-Control-Allow-Credentials', 'true');
+
+    await fetch(url, {
+      mode: 'no-cors',
+      method: "post",
+      body: payload
+    })
+      .then(console.log)
+      .catch(console.log)
+      .finally(() => {
+        message({
+          title: "Sucesso!",
+          description: "Formulário enviado com sucesso 😀"
+        });
+        return setLoading(false);
+      });
+  };
 
   return (
     <Modal
@@ -24,7 +62,7 @@ export default function RegisterModal() {
       onClose={onClose}
     >
       <ModalOverlay />
-      <ModalContent borderRadius="26px" pb={6}>
+      <ModalContent className={styles.modal} borderRadius="26px" pb={6} >
         <ModalHeader
           textAlign="center"
           color="#000"
@@ -32,18 +70,26 @@ export default function RegisterModal() {
         <ModalBody pb={6}>
           <FormControl>
             <FormLabel>Nome</FormLabel>
-            <Input 
+            <Input
+              ref={nameRef}
               border="2px solid #BDBDBD !important"
             />
           </FormControl>
 
           <FormControl mt={4}>
             <FormLabel>Celular</FormLabel>
-            <Input border="2px solid #BDBDBD !important"/>
+            <Input
+              ref={phoneRef} 
+              border="2px solid #BDBDBD !important" 
+            />
           </FormControl>
+
           <FormControl mt={4}>
             <FormLabel>E-mail</FormLabel>
-            <Input border="2px solid #BDBDBD !important"/>
+            <Input
+              ref={emailRef} 
+              border="2px solid #BDBDBD !important" 
+            />
           </FormControl>
         </ModalBody>
 
@@ -56,7 +102,8 @@ export default function RegisterModal() {
             fontWeight="400"
             color="#fff"
             margin="0 auto"
-            onClick={onClose}
+            isLoading={loading}
+            onClick={handleSubmit}
             _focus={{
               outline: "none !important"
             }}
